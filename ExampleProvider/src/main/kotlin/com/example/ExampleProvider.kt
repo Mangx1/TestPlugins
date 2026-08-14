7bb60733c57430a13e013dbb1a8d5470312ab6eb
Mangx1 <akkunbebas7777@gmail.com>
2026-08-14 14:19:37 +0700

    Build c7cf91b1d7cc87cf7e0fda813d5a75b72d6b1542

---
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+
+class ExampleProvider : MainAPI() {
+    override var mainUrl = "https://hoofoot.com"
+    override var name = "Mangx Hoofoot"
+    override val supportedTypes = setOf(TvType.Sports)
+    override var lang = "id"
+    override val hasMainPage = true
+}
