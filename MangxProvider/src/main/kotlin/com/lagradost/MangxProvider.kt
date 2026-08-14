42f65fc5936c48d931f6c5515ebd0fdcbe9368d5
Mangx1 <Akkunbebas7777@gmail.com>
2026-08-14 17:05:58 +0700

    Fix missing extractor import

--- MangxProvider/src/main/kotlin/com/lagradost/MangxProvider.kt
+++ MangxProvider/src/main/kotlin/com/lagradost/MangxProvider.kt
@@ -2,7 +2,9 @@ package com.lagradost
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
 class MangxProvider : MainAPI() {
 
