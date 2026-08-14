958a5fcb20d3b949aaac1589cc058b4dadc9e7fe
Mangx1 <akkunbebas7777@gmail.com>
2026-08-14 14:46:19 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.TvType
 class ExampleProvider : MainAPI() {
     override var mainUrl = "https://hoofoot.com"
     override var name = "Mangx Hoofoot"
-    override val supportedTypes = setOf(TvType.Sports)
+    override val supportedTypes = setOf(TvType.Live)
     override var lang = "id"
     override val hasMainPage = true
 }
